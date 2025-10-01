69d9af58c97ee579bc0ea4b492c3200213588213
subhram-rana-1 <subhram.rana@headout.com>
2025-10-01 23:46:49 +0530

    popup done completely

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,9 +3,18 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   publicDir: 'assets',
   manifest: {
-    name: 'Hello World Extension',
+    name: 'Magic comprehension with AI',
     version: '1.0.0',
-    description: 'A simple Hello World Chrome extension',
+    description: 'Sharpen your vocabulary and comprehension skill',
+    action: {
+      default_title: 'Sharpen your vocabulary and comprehension skill',
+    },
+    icons: {
+      16: 'chrome-extension-logo.png',
+      32: 'chrome-extension-logo.png',
+      48: 'chrome-extension-logo.png',
+      128: 'chrome-extension-logo.png',
+    },
   },
 });
 
